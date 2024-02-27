d53d1b2d472bf3aa0356c21ea87bc12848c88397
Harsha <158590882+Harsh-un@users.noreply.github.com>
2024-02-27 11:29:41 +0530

    Update inline.py

--- TGNMusic/plugins/bot/inline.py
+++ TGNMusic/plugins/bot/inline.py
@@ -5,8 +5,8 @@ from pyrogram.types import (
 )
 from youtubesearchpython.__future__ import VideosSearch
 
-from TGNMusic import app
-from TGNMusic.utils.inlinequery import answer
+from UNMusic import app
+from UNMusic.utils.inlinequery import answer
 from config import BANNED_USERS
 
 
