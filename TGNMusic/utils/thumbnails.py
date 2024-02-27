3fc315dc993adceb1801f7d949f84ecef709eb0f
Harsha <158590882+Harsh-un@users.noreply.github.com>
2024-02-27 10:20:28 +0530

    Update thumbnails.py

--- TGNMusic/utils/thumbnails.py
+++ TGNMusic/utils/thumbnails.py
@@ -74,14 +74,14 @@ async def get_thumb(videoid):
         logo = ImageOps.expand(logo, border=15, fill="white")
         background.paste(logo, (50, 100))
         draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("TGNMusic/assets/font2.ttf", 40)
-        font2 = ImageFont.truetype("TGNMusic/assets/font2.ttf", 70)
-        arial = ImageFont.truetype("TGNMusic/assets/font2.ttf", 30)
-        name_font = ImageFont.truetype("TGNMusic/assets/font.ttf", 30)
+        font = ImageFont.truetype("UNMusic/assets/font2.ttf", 40)
+        font2 = ImageFont.truetype("UNMusic/assets/font2.ttf", 70)
+        arial = ImageFont.truetype("UNMusic/assets/font2.ttf", 30)
+        name_font = ImageFont.truetype("UNMusic/assets/font.ttf", 30)
         para = textwrap.wrap(title, width=32)
         j = 0
         draw.text(
-            (5, 5), f"{'TGN MUSIC'}", fill="white", font=name_font
+            (5, 5), f"{'UN MUSIC'}", fill="white", font=name_font
         )
         draw.text(
             (600, 150),
